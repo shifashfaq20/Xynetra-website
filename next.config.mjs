581c6c2b92bc5544059b202318f36b32ea2d6b92
shifashfaq20 <shifashfaq20@gmail.com>
2026-07-28 18:20:14 +0500

    static export for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
